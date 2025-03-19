d47a1d692507fb31c108119f6f0e75d8f498c91d
Rafi-Shariar <rafi.shariar619@gmail.com>
2025-03-19 13:42:17 +0600

    working on user dashbord

--- scripts/login.js
+++ scripts/login.js
@@ -1,14 +1,19 @@
-document.getElementById('RegistratonBtn').addEventListener('click',(e)=>{
+
+
+
+//Handle Login As
+document.getElementById('LoginBtn').addEventListener('click', (e)=>{
+
     e.preventDefault();
 
+    const loginAs = document.getElementById('loginas').value;
+    console.log(loginAs);
+
+    if(loginAs == 'User'){
+        window.location.href = './Dashboards/User/UserDashboard.html';
+    }
+    else{
+        window.location.href = './Dashboards/AdminDashboard.html';
+    }
     
-
-    document.getElementById('loginForm').classList.add('hidden');
-    document.getElementById('registrationForm').classList.remove('hidden');
-
-})
-
-
-document.getElementById('createBtn').addEventListener('click',(e)=>{
-    alert("Account Created Successfully")
 })
